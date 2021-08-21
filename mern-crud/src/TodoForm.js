8f03b1597c9c7080ce3d435f9a51738c6bc8d8fa
alethrome <49285810+alethrome@users.noreply.github.com>
2021-08-21 14:57:39 +0800

    Include backend

--- mern-crud/src/TodoForm.js
+++ mern-crud/src/TodoForm.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import { useForm } from "react-hook-form";
-import { useHistory } from "react-router-dom";
 
 export const TodoForm = ({ todo, onSubmit }) => {
     const {register, handleSubmit} = useForm({
         defaultValues: { text: todo ? todo.text : ""},
     });
-    const history = useHistory();
 
     const submitHandler = handleSubmit((data) => {
         onSubmit(data);
-        history.push("/");
     });
 
     return (
@@ -25,7 +22,7 @@ export const TodoForm = ({ todo, onSubmit }) => {
             </div>
             <div className="form-group">
                 <button type="submit" className="btn btn-primary">
-                    Create Todo
+                    Save Todo
                 </button>
             </div>
         </form>

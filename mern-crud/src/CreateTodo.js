1736efc47460d9de31a9bfec01629737da50345e
alethrome <49285810+alethrome@users.noreply.github.com>
2021-08-21 12:16:16 +0800

    Create and edit forms
    --render the list
    --alert prompt for the input value
    --set predefined value in the Edit input field

--- mern-crud/src/CreateTodo.js
+++ mern-crud/src/CreateTodo.js
@@ -1,5 +1,17 @@
 import React from 'react';
+import {TodoForm} from "./TodoForm";
 
 export const CreateTodo = () => {
-    return <div className="container">Create Todo</div>
+    const onSubmit = (data) => {
+        alert(JSON.stringify(data));
+    };
+
+    return (
+        <div className="container">
+            <div className="mt-3">
+                <h3>Create Todo</h3>
+                <TodoForm onSubmit={onSubmit}/>
+            </div>
+        </div>
+    )
 }
